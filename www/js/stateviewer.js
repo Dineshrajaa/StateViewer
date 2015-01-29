f028d6dc9417cf487970cc5440b3d6bf106c5a3b
Dineshrajaa <dineshrajaa.93@gmail.com>
2015-01-29 15:17:17 +0530

    Lazy Loading

--- www/js/stateviewer.js
+++ www/js/stateviewer.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	var webserviceFile="http://services.groupkt.com/country/get/all";
 	var countryNames=[];
+
+			/*Country Listing Methods*/
 	function readCountries(){
 		//Read the CountryNames from Webservice using AJAX Call			
 		$.ajax({
@@ -28,16 +30,63 @@ $(document).ready(function(){
 	function listCountries(){
 		//Lists the CountryNames from Webservice
 		
-		for(var j=0;j<countryNames.length;j++){
-			$("#countryList").append("<li id='"+j+"'><a href='#'>"+countryNames[j]+"</a></li>")
+		for(var j=0;j<countryNames.length-234;j++){
+			$("#countryList").append("<li id='"+j+"'><a href='#'>"+countryNames[j]+"</a></li>");
 		}
 		$("#countryList").listview("refresh");
+		//alert(("#countryList li").length);
 		
 	}
 
-	$("#searchpgbtn").tap();
+			/**Testing Methods**/
+		function scrollChecker(){
+			
+			var currentPage=$(':mobile-pagecontainer').pagecontainer('getActivePage')[0];			
+			var pageHeight=$(window).height();
+			var contentHeight =$(".ui-content",currentPage).outerHeight();
+			var headerSize=$(".ui-header", currentPage).outerHeight()-1;
+			var footerSize=$(".ui-footer", currentPage).outerHeight()-1;
+			var scrolled = $(window).scrollTop();
+			var scrollEnd = contentHeight-pageHeight+headerSize+footerSize;
+			/*alert("PageHeight "+pageHeight);
+			alert("contentHeight "+contentHeight);
+			alert("HeaderSize "+headerSize);
+			alert("FooterSize "+footerSize);
+			alert("Scrolled "+scrolled);
+			alert("Scrollend "+scrollEnd);*/
+			if (currentPage.id=="home-page"&&scrolled >= scrollEnd) {
+				addListItems(currentPage);
+			}
+		}
+
+		function addListItems(presentPage){
+			$(document).off("scrollstop");
+			$.mobile.loading("show", {
+    		text: "Loading..",
+   			textVisible: true
+  			});
+  			setTimeout(function() {
+    		var listLength=$("li",presentPage).length;
+    		var newListLength=listLength+5;
+    
+    for (var k = listLength; k < newListLength; k++) {
+      $("#countryList").append("<li id='"+k+"'><a href='#'>"+countryNames[k]+"</a></li>");
+    }
+   $("#countryList").listview("refresh");
+    $.mobile.loading("hide");
+    $(document).on("scrollstop", scrollChecker);
+  }, 500);
+  			
+		}
+
+		
+			/**End of Testing Methods**/
+
+	
 
 	//Function Calls
 	readCountries();
+	$(document).on("scrollstop", scrollChecker);
+	
 	//Loaded all the elements into DOM
 });
